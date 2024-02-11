84ee3b85e00edfbe712119fb9737b23a2d503a6b
Yummimi <sarah.tiblin@gmail.com>
2024-02-11 23:22:21 +0100

    Updates Gitignore

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,7 +13,7 @@ var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
         services.AddDbContext<DataContext>(options =>
             options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\School\Databases\MyDatabaseProject\ClassLibrary\Data\Database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
 
-        services.AddScoped<AddressRepository>();
+        /*services.AddScoped<AddressRepository>();
         services.AddScoped<CategoryRepository>();
         services.AddScoped<ProductRepository>();
         services.AddScoped<UserRepository>();
@@ -28,26 +28,26 @@ var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
         services.AddSingleton<ConsoleUI_Products>();
         services.AddSingleton<ConsoleUI_Users>();
         services.AddSingleton<ConsoleUI_Categories>();
-        services.AddSingleton<ConsoleUI_Profiles>();
+        services.AddSingleton<ConsoleUI_Profiles>();*/
 
     }).Build();
 
-//Jag hann ej göra en user-interface eller en "meny", so kör dessa i den ordning som du önskar. Jag kommenterar ut alla så länge:
+//Jag hann ej göra en user-interface eller en "meny", so kör dessa i den ordning som du önskar. Jag kommenterar ut alla så länge.
 
-var consoleUIProducts = builder.Services.GetRequiredService<ConsoleUI_Products>();
+//var consoleUIProducts = builder.Services.GetRequiredService<ConsoleUI_Products>();
 //consoleUIProducts.CreateProduct_UI();
 //consoleUI.GetProducts_UI();
 //consoleUI.UpdateProduct_UI();
 //consoleUI.DeleteProduct_UI();
 
-var consoleUIUsers = builder.Services.GetRequiredService<ConsoleUI_Users>();
+//var consoleUIUsers = builder.Services.GetRequiredService<ConsoleUI_Users>();
 //consoleUIUsers.CreateUser_UI();
 //consoleUIUsers.GetAllUsers_UI();
 //consoleUIUsers.GetUsersByEmail_UI();
-consoleUIUsers.UpdateUser_UI();
+//consoleUIUsers.UpdateUser_UI();
 //consoleUIUsers.DeleteUser_UI();
 
-var consoleUICategories = builder.Services.GetRequiredService<ConsoleUI_Categories>();
+//var consoleUICategories = builder.Services.GetRequiredService<ConsoleUI_Categories>();
 //consoleUICategories.CreateCategory_UI();
 //consoleUICategories.GetCategories_UI();
 //consoleUICategories.UpdateCategory_UI();
